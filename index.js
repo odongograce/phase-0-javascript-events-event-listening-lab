5de4cc86e6ebaf24ec0bd02b4ec1db9c9f3894eb
Grace Odongo <graceakinyi024@gmail.com>
2025-09-25 14:32:37 +0300

    updates

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function addingEventListener() {
-    let input = document.getElementById('button');
-    input.addEventListener('click', function() {
-    alert('Button clicked');
-  });
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
